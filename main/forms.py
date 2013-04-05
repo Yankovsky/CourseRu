da15dac2cb0169ebb1a69511216e180b64a1164b
avrybintsev <avrybintsev@gmail.com>
2013-04-05 09:45:07 +0400

    feedback form fix

--- main/forms.py
+++ main/forms.py
@@ -12,5 +12,5 @@ class FeedbackForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(FeedbackForm, self).__init__(*args, **kwargs)
         self.fields['body'].widget.attrs = {'rows': 6, 'class': 'span6', 'placeholder': u'Текст сообщения'}
-        for field in self.Meta.fields:
+        for field in self.fields:
             self.fields[field].error_messages = {'required' : u'Необходимое поле', 'invalid': u'Некоректный формат данных'}
