2dabbf45dfdad1a7ee012420eb4fec6c49740b9c
avrybintsev <avrybintsev@gmail.com>
2013-04-08 14:36:49 +0400

    file input big #2 fixed

--- CourseRu/widgets.py
+++ CourseRu/widgets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.forms import widgets
 from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 
 class FileUploadWidget(widgets.FileInput):
@@ -8,6 +9,9 @@ class FileUploadWidget(widgets.FileInput):
         # gets id if it exists
         id_part = u''
         if attrs and 'id' in attrs:
-            id_part = u'id='+attrs['id']
+            id_part = attrs['id']
+            if (id_part[0] != '"'):
+                id_part = mark_safe(u'id="' + id_part + u'"')
 
         return render_to_string('forms/fileupload.html', {'id': id_part, 'name': name})
+
